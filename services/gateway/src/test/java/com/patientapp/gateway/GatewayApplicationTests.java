96c877c60fb54f86f326c6b1ed20ef5a988a88bb
Ovett Mora <ovettmora17@gmail.com>
2025-08-21 22:53:36 -0600

    Add initial setup for gateway service with Maven configuration and application properties

---
+++ services/gateway/src/test/java/com/patientapp/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patientapp.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
