83bf530acf738f19429bf102f34871ed88f28678
Ovett Mora <ovettmora17@gmail.com>
2025-08-21 21:06:43 -0600

    Add PermissionRepository and RoleAndPermissionInitializer for role and permission management

---
+++ services/auth-service/src/main/java/com/patientapp/authservice/repository/PermissionRepository.java
@@ -0,0 +1,13 @@
+package com.patientapp.authservice.repository;
+
+
+import com.patientapp.authservice.entity.Permission;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PermissionRepository extends JpaRepository<Permission, Integer> {
+    Optional<Permission> findByName(String name);
+}
