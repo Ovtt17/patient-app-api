a4f6e3321479ec21c388c68c6e9439de2fadec73
Ovett Mora <ovettmora17@gmail.com>
2025-08-21 22:43:45 -0600

    Update SecurityConfig to allow access to Swagger UI and API documentation endpoints

--- services/auth-service/src/main/java/com/patientapp/authservice/config/security/SecurityConfig.java
+++ services/auth-service/src/main/java/com/patientapp/authservice/config/security/SecurityConfig.java
@@ -44,7 +44,17 @@ public class SecurityConfig {
                                         "/auth/login",
                                         "/auth/activate-account",
                                         "/auth/refresh",
-                                        "/auth/logout"
+                                        "/auth/logout",
+                                        "/v2/api-docs",
+                                        "/v3/api-docs",
+                                        "/v3/api-docs/**",
+                                        "/swagger-resources",
+                                        "/swagger-resources/**",
+                                        "/configuration/ui",
+                                        "/configuration/security",
+                                        "/swagger-ui/**",
+                                        "/webjars/**",
+                                        "/swagger-ui.html"
                                 ).permitAll()
                                 .requestMatchers(
                                         HttpMethod.GET,
