b140b810ffda5e72fa7668fd4084d47a76005c46
Ovett Mora <ovettmora17@gmail.com>
2025-08-27 21:52:13 -0600

    feat: add first_name, last_name, and temporary_password fields to users table

--- services/auth-service/src/main/resources/db/migration/V1__init_auth_database.sql
+++ services/auth-service/src/main/resources/db/migration/V1__init_auth_database.sql
@@ -33,9 +33,12 @@ CREATE TABLE tokens
 CREATE TABLE users
 (
     id                 BINARY(16)   NOT NULL,
+    first_name         VARCHAR(255) NOT NULL,
+    last_name          VARCHAR(255) NULL,
     email              VARCHAR(255) NOT NULL,
     username           VARCHAR(255) NOT NULL,
     password           VARCHAR(255) NULL,
+    temporary_password VARCHAR(255) NULL,
     provider           VARCHAR(255) NOT NULL,
     phone              VARCHAR(15)  NULL,
     profile_picture    TEXT         NULL,
