2d52add04c107acfcb8f2e72a957ea2373ea815b
Ovett Mora <ovettmora17@gmail.com>
2025-09-12 12:51:10 -0600

    feat: add Schedule entity for managing doctor schedules

---
+++ services/doctor-service/src/main/java/com/patientapp/doctorservice/modules/doctor/entity/Schedule.java
@@ -0,0 +1,41 @@
+package com.patientapp.doctorservice.modules.doctor.entity;
+
+import com.patientapp.doctorservice.common.model.BaseAuditingEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.DayOfWeek;
+import java.time.Instant;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "schedules")
+public class Schedule extends BaseAuditingEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private DayOfWeek dayOfWeek;
+
+    @Column(nullable = false)
+    private Instant startTime;
+
+    @Column(nullable = false)
+    private Instant endTime;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "doctor_id")
+    private Doctor doctor;
+
+}
