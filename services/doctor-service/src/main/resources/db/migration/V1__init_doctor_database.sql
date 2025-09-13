55877f6f3bbb4a4ec783a957fd8308e9b7c0e779
Ovett Mora <ovettmora17@gmail.com>
2025-09-13 00:49:31 -0600

    feat: add doctor_unavailabilities table and foreign key constraint for doctor references

--- services/doctor-service/src/main/resources/db/migration/V1__init_doctor_database.sql
+++ services/doctor-service/src/main/resources/db/migration/V1__init_doctor_database.sql
@@ -5,6 +5,20 @@ CREATE TABLE doctor_specialties
     CONSTRAINT pk_doctor_specialties PRIMARY KEY (doctor_id, specialty_id)
 );
 
+CREATE TABLE doctor_unavailabilities
+(
+    id                 INT AUTO_INCREMENT NOT NULL,
+    created_by         VARCHAR(255)       NOT NULL,
+    last_modified_by   VARCHAR(255)       NULL,
+    created_date       datetime           NOT NULL,
+    last_modified_date datetime           NULL,
+    start_time         datetime           NOT NULL,
+    end_time           datetime           NOT NULL,
+    available          BIT(1)             NOT NULL,
+    doctor_id          BINARY(16)         NOT NULL,
+    CONSTRAINT pk_doctor_unavailabilities PRIMARY KEY (id)
+);
+
 CREATE TABLE doctors
 (
     id                 BINARY(16)   NOT NULL,
@@ -50,6 +64,9 @@ ALTER TABLE doctors
 
 ALTER TABLE specialties
     ADD CONSTRAINT uc_specialties_name UNIQUE (name);
+
+ALTER TABLE doctor_unavailabilities
+    ADD CONSTRAINT FK_DOCTOR_UNAVAILABILITIES_ON_DOCTOR FOREIGN KEY (doctor_id) REFERENCES doctors (id);
 
 ALTER TABLE schedules
     ADD CONSTRAINT FK_SCHEDULES_ON_DOCTOR FOREIGN KEY (doctor_id) REFERENCES doctors (id);
