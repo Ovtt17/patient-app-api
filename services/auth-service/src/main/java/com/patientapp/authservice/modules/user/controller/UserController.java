9de9f6fce9216d9f5f5feaa404ae4e2e6b36a8fc
Ovett Mora <ovettmora17@gmail.com>
2025-09-21 18:38:33 -0600

    feat: add OpenAPI operation annotation for retrieving user by ID

--- services/auth-service/src/main/java/com/patientapp/authservice/modules/user/controller/UserController.java
+++ services/auth-service/src/main/java/com/patientapp/authservice/modules/user/controller/UserController.java
@@ -2,13 +2,11 @@ package com.patientapp.authservice.modules.user.controller;
 
 import com.patientapp.authservice.modules.user.dto.UserResponseDTO;
 import com.patientapp.authservice.modules.user.service.interfaces.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -19,6 +17,7 @@ import java.util.UUID;
 public class UserController {
     private final UserService service;
 
+    @Operation(summary = "Obtener usuario por ID", description = "Obtiene los detalles de un usuario específico mediante su ID.")
     @GetMapping("/{userId}")
     public ResponseEntity<UserResponseDTO> getUserById(@PathVariable UUID userId) {
         return ResponseEntity.ok(service.getUserById(userId));
