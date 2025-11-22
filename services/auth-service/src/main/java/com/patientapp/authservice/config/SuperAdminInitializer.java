5b1cfc680b4e9060bad125cd29433d3bcb39feed
Ovett Mora <ovettmora17@gmail.com>
2025-11-21 21:57:39 -0600

    feat: update role and permission initialization order and enhance user registration authorization

--- services/auth-service/src/main/java/com/patientapp/authservice/config/SuperAdminInitializer.java
+++ services/auth-service/src/main/java/com/patientapp/authservice/config/SuperAdminInitializer.java
@@ -3,9 +3,11 @@ package com.patientapp.authservice.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 
 @Configuration
 @RequiredArgsConstructor
+@Order(2)
 public class SuperAdminInitializer implements CommandLineRunner {
     private final SuperAdminService superAdminService;
 
